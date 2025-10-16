b9a8cda9602da21590c475f7b9eee94f50f2a249
ewl5814 <ledermanelijah@gmail.com>
2025-10-15 20:34:54 -0400

    c

---
+++ cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String fur;
+    private int lives;
+
+    public Cat(String fur, int lives) {
+        this.fur = fur;
+        this.lives = lives;
+    }
+
+    public String furball() {
+        return "Yack";
+    }
+}
